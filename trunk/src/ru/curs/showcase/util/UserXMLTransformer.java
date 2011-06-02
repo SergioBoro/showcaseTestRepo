bd982fef7985128b5f5923f385911d27d130c274
d.orekhov <orekhoff@gmail.com>
2011-06-02 11:01:20 +0000

    быстро сделал нормальную обработку неXML файлов при закачке в XForms
    git-svn-id: https://share.curs.ru/svn/showcase/tags/v1.5.5@1757 322482b4-72a2-ae44-8c81-ef1a4f415bef

--- trunk/src/ru/curs/showcase/util/UserXMLTransformer.java
+++ trunk/src/ru/curs/showcase/util/UserXMLTransformer.java
@@ -2,7 +2,13 @@ package ru.curs.showcase.util;
 
 import java.io.*;
 
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import ru.curs.showcase.app.api.datapanel.DataPanelElementProc;
+import ru.curs.showcase.exception.NotXMLException;
 import ru.curs.showcase.model.DataFile;
 
 /**
@@ -37,14 +43,15 @@ public final class UserXMLTransformer {
 	 * Проверяет исходник и трансформирует его если в процедуре трансформации
 	 * описаны соответствующие схема и файл трансформации.
 	 * 
-	 * @throws UnsupportedEncodingException
+	 * @throws IOException
 	 */
-	public void checkAndTransform() throws UnsupportedEncodingException {
+	public void checkAndTransform() throws IOException {
 		if ((subject == null) || (proc == null)) {
 			return;
 		}
 
 		if (proc.getSchemaName() != null) {
+			checkForXML();
 			XMLValidator validator = new XMLValidator(new UserDataXSDSource());
 			InputStream is = StreamConvertor.outputToInputStream(subject.getData());
 			validator.validate(new XMLSource(is, subject.getName(), proc.getSchemaName()));
@@ -54,6 +61,20 @@ public final class UserXMLTransformer {
 			String res = XMLUtils.xsltTransform(is, proc.getTransformName());
 			result = TextUtils.stringToStream(res);
 		}
+	}
+
+	private Document checkForXML() throws IOException {
+		DocumentBuilder db = XMLUtils.createBuilder();
+		Document doc = null;
+		InputStream is = StreamConvertor.outputToInputStream(subject.getData());
+		try {
+			doc = db.parse(is);
+		} catch (SAXException e) {
+			throw new NotXMLException(e, subject.getName());
+		} catch (IOException e) {
+			throw e;
+		}
+		return doc;
 	}
 
 	public DataFile<ByteArrayOutputStream> getSubject() {
